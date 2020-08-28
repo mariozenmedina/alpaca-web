60b19de035a0107e6d726eb1e871675237f0e434
Mário Veronesi <marioinfo@oi.com.br>
2020-08-27 23:05:33 -0300

    finalizado

--- header.php
+++ header.php
@@ -1,26 +1,24 @@
 <!DOCTYPE html>
 <html lang="pt" xml:lang="pt">
-<title></title>
+<title>Alpaca WEB - Design &amp; Programação</title>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <!-- INÍCIO DOS METAS -->
-<meta name="generator" content="WordPress <?php //bloginfo('version'); ?>">
-<meta charset="<?php //bloginfo('charset'); ?>">
+<meta charset="UTF-8">
 <meta http-equiv="Content-Language" content="pt-br">
-<meta name="url" content="<?php //bloginfo('url'); ?>">
-<meta name="description" content="">
-<meta name="keywords" content="">
-<meta name="reply-to" content="">
-<meta name="copyright" content="">
+<meta name="url" content="http://alpacaweb.com.br">
+<meta name="description" content="Agência WEB com 10 anos de mercado, desenvolvendo SITES E SISTEMAS responsivos e com o que há de mais atualizado em DESIGN E PROGRAMAÇÃO. Trabalhamos com critérios rigorosos e entregamos o melhor que a internet pode oferecer.">
+<meta name="keywords" content="design, desenvolvimento, programação, publicidade">
+<meta name="reply-to" content="mario@alpacaweb.com.br">
 <meta name="category" content="Internet">
 <meta name="Robots" content="Index,Follow">
-<meta name="author" content="">
-<meta property="og:site_name" content=""/>
+<meta name="author" content="Alpaca WEB">
+<meta property="og:site_name" content="Alpaca WEB"/>
 <meta property="og:type" content="website"/>
-<meta property="og:title" content=""/>
-<meta property="og:description" content=""/>
-<meta property="og:url" content=""/>
-<meta property="og:image" content=""/>
+<meta property="og:title" content="DESIGN & PROGRAMAÇÃO"/>
+<meta property="og:description" content="Agência WEB com 10 anos de mercado, desenvolvendo SITES E SISTEMAS responsivos e com o que há de mais atualizado em DESIGN E PROGRAMAÇÃO. Trabalhamos com critérios rigorosos e entregamos o melhor que a internet pode oferecer."/>
+<meta property="og:url" content="http://alpacaweb.com.br"/>
+<meta property="og:image" content="http://alpacaweb.com.br/img/og-image.png"/>
 <meta name="HandheldFriendly" content="True" />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 <!-- FIM DOS METAS -->
