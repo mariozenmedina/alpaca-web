9fa3b246fa175ba0583e40c4d5a0eeff8c1444d8
mariozenmedina <mazen.mario@gmail.com>
2024-01-12 03:24:31 -0300

    backup github

--- header.php
+++ header.php
@@ -25,5 +25,17 @@
 <link rel="shortcut icon" href="./img/icon.png" />
 <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@600&family=Raleway:wght@800&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="./css/less.css" type="text/css" media="all">
+<!-- GOOGLE -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-33275562-9', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<!-- GOOGLE -->
 </head>
 <body>
